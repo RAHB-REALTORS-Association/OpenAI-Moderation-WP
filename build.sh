54038eb69f7939eb444a37a9e310d87a21790647
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-03-22 00:52:51 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@
 version=$(grep "^Version:" openai-moderation.php | awk '{print $2}')
 
 # Create the plugin package
-cd ..
-zip -r openai-moderation-$version.zip openai-moderation -x "*.git*" -x "*.DS_Store" -x "*build.sh*"
+zip -r openai-moderation-$version.zip . -x "*.git*" -x "*.DS_Store" -x "*build.sh*" -x "*dist*"
 
 # Move the plugin package to the dist directory
-mv openai-moderation-$version.zip openai-moderation/dist/
+mv openai-moderation-$version.zip dist/
